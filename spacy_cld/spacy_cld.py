b2279e8f68f6f40e380ebcdacb5c7c8c32108b89
Leonard Lausen <leonard@lausen.nl>
2018-01-30 18:57:28 +0800

    Fix #1

--- spacy_cld/spacy_cld.py
+++ spacy_cld/spacy_cld.py
@@ -1,3 +1,5 @@
+import logging
+
 from pycld2 import detect
 from spacy.tokens import Doc, Span
 
@@ -17,7 +19,10 @@ def get_scores(text, cld_results=None):
 
 
 def detect_languages(text):
-    _, _, results = detect(text.text)
+    try:
+        _, _, results = detect(text.text)
+    except:
+        results = [[None, "error", 0.0, None]]
     return results
 
 
